08d36b062b580fd384a11f13d16753d2a2e8ef11
Simone Ruffini <simone.ruffini@tutanota.com>
2020-06-29 18:20:43 +0200

    rewriting of NVRegEmu

--- src/nv_reg_emu.vhd
+++ src/nv_reg_emu.vhd
@@ -36,12 +36,13 @@ use work.COMMON_PACKAGE.all;
 
 entity nv_reg_emu is
     Generic(
-        NV_EMU_MAX_CLK: INTEGER -- this is the maximum clk that the nv_reg will handle without latency
+        MAX_DELAY: INTEGER -- this is the maximum delay that the nv_reg uses to process data
     );
     Port ( 
-        clk : IN STD_LOGIC;
-        rst : IN STD_LOGIC; 
-        clk_out: OUT STD_LOGIC
+        clk     : IN STD_LOGIC;
+        resetN  : IN STD_LOGIC;
+        load_en : IN STD_LOGIC; 
+        busy    : OUT STD_LOGIC
     );
 end nv_reg_emu;
 
@@ -49,90 +50,41 @@ architecture Behavioral of nv_reg_emu is
     ----------------------------------------------------
     --      INTERNAL SIGNALS FOR THE SAMPLING DELAY
     ----------------------------------------------------
+    constant counter_end_value : INTEGER := get_busy_counter_end_value(MASTER_CLK_PERIOD_NS, MAX_DELAY);
+
     signal internal_TC : STD_LOGIC;
-    constant internal_prescaler_max_value : INTEGER := get_prescaler_value(input_clk=>MASTER_CLK_SPEED, output_clk=>NV_EMU_MAX_CLK);
+    signal internal_INIT: STD_LOGIC;
     signal internal_CE: STD_LOGIC;
-    signal internal_clk_out: STD_LOGIC;
+    
 begin
-    --internal_prescaler_max_value <= get_prescaler_value(input_clk=>MASTER_CLK_SPEED, output_clk=>NV_EMU_MAX_CLK);
-    
-    --check if the prescaler is on or off
-    internal_CE <=  '0' when internal_prescaler_max_value  <= 1 else '1';
-    
-    -- if the prescaler is off then just use the internal clk
-    clk_out <= internal_clk_out when internal_CE = '1' else clk;
-   
-    
-    prescaler : entity work.counter(Behavioral)
+
+    BUSY_COUNTER : entity work.counter(Behavioral)
     generic map(
-        MAX => internal_prescaler_max_value-1,
+        MAX => counter_end_value,
         INIT_VALUE => 0,
         INCREASE_BY => 1
     )
     port map(
         clk => clk,
-        resetn => rst,
-        INIT => '0', --never init the counter
+        resetn => resetN,
+        INIT => internal_INIT,
         CE => internal_CE,
         TC => internal_TC
         --value=> internal_value
     );
     
-    process (clk,rst) begin
-        if(rst='0') then
-            internal_clk_out <= '0';
-        elsif(rising_edge(clk) AND internal_TC = '1') then
-            internal_clk_out <= not internal_clk_out;
+    busy <= not internal_TC;
+    process (clk,resetN) begin
+        if(resetN='0') then
+            internal_INIT <='0';
+        elsif(rising_edge(clk)) then
+            if(load_en = '1') then
+                internal_CE <= '1';
+            end if;
+            if(load_en = '0' and internal_TC = '1') then
+                internal_CE <= '0';
+            end if;
         end if;
     end process;
     
 end Behavioral;
-
-architecture Behavioral1 of nv_reg_emu is
-
-    ----------------------------------------------------
-    --      INTERNAL SIGNALS FOR THE SAMPLING DELAY
-    ----------------------------------------------------
-    signal internal_TC : STD_LOGIC;
-    constant internal_prescaler_max_value : INTEGER := get_prescaler_value(input_clk=>MASTER_CLK_SPEED, output_clk=>NV_EMU_MAX_CLK);
-    signal internal_CE: STD_LOGIC;
-    signal internal_clk_out: STD_LOGIC;
-    signal busy: STD_LOGIC;
-    
-begin
-    --internal_prescaler_max_value <= get_prescaler_value(input_clk=>MASTER_CLK_SPEED, output_clk=>NV_EMU_MAX_CLK);
-    
-    --check if the prescaler is on or off
-    internal_CE <=  '0' when internal_prescaler_max_value  <= 1 else '1';
-    
-    -- if the prescaler is off then just use the internal clk
-    clk_out <= internal_clk_out when internal_CE = '1' else clk;
-   
-    -- If the internal_CE (the prescaler) is off then the nv_reg is never busy
-    -- But if it is on and the internal_TC is on (happens 2 times per internal_clk_out) 
-    -- and we are in the down period of the clock then we are not in busy
-    busy <= internal_CE AND (NOT (internal_TC AND (NOT internal_clk_out)));
-   
-    prescaler : entity work.counter(Behavioral)
-    generic map(
-        MAX => internal_prescaler_max_value-1,
-        INIT_VALUE => 0,
-        INCREASE_BY => 1
-    )
-    port map(
-        clk => clk,
-        resetn => rst,
-        INIT => '0', --never init the counter
-        CE => internal_CE,
-        TC => internal_TC
-        --value=> internal_value
-    );
-    process (clk,rst) begin
-        if(rst='0') then
-            internal_clk_out <= '0';
-        elsif(rising_edge(clk) AND internal_TC = '1') then
-            internal_clk_out <= not internal_clk_out;
-        end if;
-    end process;
-    
-end Behavioral1;
