7687892f390a0e376b5b0ba7ce20c72041ad09d3
lucacaronti <carontiluca@gmail.com>
2020-07-02 16:50:48 +0200

    manually merged COMMON_PACKAGE and ARCHITECTURE_PACKAGE into src/packages

--- src/packages.vhd
+++ src/packages.vhd
@@ -50,3 +50,57 @@ package NV_REG_EMULATOR_PKG is
         data_recovered_state
     );
 end package NV_REG_EMULATOR_PKG;
+
+package ARCHITECTURE_PACKAGE is
+    type fsm_nv_reg_state_t is(
+        shutdown_s,
+        init_s,
+        start_data_recovery_s,
+        recovery_s,
+        data_recovered_s,
+        do_operation_s,
+        start_data_save_s,
+        data_save_s,
+        data_saved_s
+        );
+        
+    type threshold_t is(
+        hazard,
+        waring,
+        nothing
+    );
+    
+end package;
+
+package COMMON_PACKAGE is
+        type PWR_STATES_ARR_TYPE is array(natural range <>) of INTEGER;
+       
+        constant NV_REG_WIDTH: INTEGER := 4;
+        constant MASTER_CLK_SPEED : INTEGER := 100000;
+        constant MASTER_CLK_PERIOD_NS : INTEGER := (1e9/MASTER_CLK_SPEED);
+
+        constant FRAM_MAX_DELAY_NS : INTEGER := MASTER_CLK_PERIOD_NS *4;
+        
+        pure function get_busy_counter_end_value(
+            input_clk_period : INTEGER;  --in nannoseconds
+            max_delay_time: INTEGER  --in nannoseconds
+        ) return INTEGER;
+end package;
+
+library IEEE;
+use ieee.math_real.all;
+package body COMMON_PACKAGE is 
+    pure function get_busy_counter_end_value(
+            input_clk_period : INTEGER;
+            max_delay_time: INTEGER 
+        ) return INTEGER is
+    begin
+        assert (max_delay_time > 0)
+            report "nv_reg dealy time = 0" severity Failure;
+        if(max_delay_time <= input_clk_period) then--if the fram is as fast as the clk or faster 
+            return 0; --then nothing to do 
+        else
+            return  integer(ceil(real(max_delay_time)/real(input_clk_period))) - 1; --return the uper bound if the delay is grather then the clk period
+        end if;
+    end function;
+end package body COMMON_PACKAGE;
