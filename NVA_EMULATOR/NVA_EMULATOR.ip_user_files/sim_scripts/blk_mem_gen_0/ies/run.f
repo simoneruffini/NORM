e9988c89804f0d6f287012ec6b090484694bd3fb
Simone Ruffini <simone.ruffini@tutanota.com>
2020-07-02 15:02:41 +0200

    renamed generic port

--- NVA_EMULATOR/NVA_EMULATOR.ip_user_files/sim_scripts/blk_mem_gen_0/ies/run.f
+++
@@ -1,4 +0,0 @@
--makelib ies_lib/xil_defaultlib \
-  "../../../../NVA_EMULATOR.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_sim_netlist.vhdl" \
--endlib
-
--- NVA_EMULATOR/NVA_EMULATOR.ip_user_files/sim_scripts/blk_mem_gen_0/questa/elaborate.do
+++
@@ -1,1 +0,0 @@
-vopt -64 +acc -l elaborate.log -L xil_defaultlib -L secureip -work xil_defaultlib xil_defaultlib.blk_mem_gen_0 -o blk_mem_gen_0_opt
